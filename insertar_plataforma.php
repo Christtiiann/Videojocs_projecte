0cfe4fae720bc3328d4910cba401b4bd735cfcda
Christtiiann <150262755+Christtiiann@users.noreply.github.com>
2024-02-14 16:49:39 +0100

    Add files via upload

---
+++ insertar_plataforma.php
@@ -0,0 +1,49 @@
+<?php
+
+
+
+
+
+
+class insertar {
+private $servername="127.0.0.1";
+private $username="root";
+private $password="iesmanacor";
+
+ private $conn;
+ private $jsonData;
+ private $data;
+
+    public function __construct() {
+        try {
+            $this->conn = new PDO("mysql:host=$this->servername;dbname=videojocs_projecte", $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected successfully";
+            $this->jsonData = file_get_contents("dades.json");
+            $this->data = json_decode($this->jsonData, true);
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+    }
+    public function inserir() {
+        try {
+            // var_dump($this->data);
+            foreach ($this->data as $key => $videojuego) {
+                if ($key !== "plataforma" && $videojuego !== null) {
+                    $plataforma = $videojuego['Plataforma'];
+                    $sql = "INSERT INTO plataforma (nom) VALUES (:nom)";
+                    $stmt = $this->conn->prepare($sql);
+                    $stmt->bindParam(':nom', $plataforma);
+                    $stmt->execute();
+                }
+            }
+            echo "Inserció realitzada amb èxit";
+        } catch (PDOException $e) {
+            echo "Error en l'inserció: " . $e->getMessage();
+        }
+    }
+}
+
+$insertar = new insertar();
+$insertar->inserir();
+?>
